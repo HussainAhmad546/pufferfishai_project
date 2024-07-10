433348d8bd8436686c739d7976df7f0da3e05bcc
HussainAhmad546 <syedh2958@gmail.com>
2024-07-10 12:58:58 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-white bg-black py-2 px-2">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
